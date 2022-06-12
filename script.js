acfa1e29b911b35e9d0a7d789ff06a729e5c7e98
Gokul0201 <105444440+Gokul0201@users.noreply.github.com>
2022-06-12 19:00:09 +0530

    files added

---
+++ script.js
@@ -0,0 +1,37 @@
+let count1=10;
+setTimeout(()=>{
+       document.querySelector(".count").innerHTML=count1;
+       setTimeout(()=>{
+        document.querySelector(".count").innerHTML=--count1;
+        setTimeout(()=>{
+            document.querySelector(".count").innerHTML=--count1;
+            setTimeout(()=>{
+                document.querySelector(".count").innerHTML=--count1;
+                setTimeout(()=>{
+                    document.querySelector(".count").innerHTML=--count1;
+                    setTimeout(()=>{
+                        document.querySelector(".count").innerHTML=--count1;
+                        setTimeout(()=>{
+                            document.querySelector(".count").innerHTML=--count1;
+                            setTimeout(()=>{
+                                document.querySelector(".count").innerHTML=--count1;
+                                setTimeout(()=>{
+                                    document.querySelector(".count").innerHTML=--count1;
+                                    setTimeout(()=>{
+                                        document.querySelector(".count").innerHTML=--count1;
+                                        setTimeout(()=>{
+                                            document.querySelector(".count").innerHTML=--count1;  
+                                         setTimeout(()=>{
+                                            document.querySelector(".count").innerHTML="Happy Independence Day...!";
+                                             },1000)
+                                         },1000)
+                                     },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
